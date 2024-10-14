1c3e9ce07a1b79997c15df251075166982e09faf
bizzy604 <kevinyamatta50@gmail.com>
2024-10-14 06:51:54 -0700

    Task 0 update

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Module: Task 0"""
+
 import asyncio
 import random
 
@@ -18,4 +19,4 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
-    return wait_time
+    return wait_time \n
